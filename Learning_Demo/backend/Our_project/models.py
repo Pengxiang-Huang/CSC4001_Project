6d1641a49e996f5aeaffbe894e7effaece25d178
Demario-Huang <119010108@link.cuhk.edu.cn>
2022-03-03 11:02:42 +0800

    merge demo

---
+++ Learning_Demo/backend/Our_project/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+# describe the Database
+class students(models.Model):
+    user_id = models.CharField(max_length=10)
+    passcode = models.CharField(max_length=20)
+
